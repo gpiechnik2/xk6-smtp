cfceb7882208a29c757962668b9212acb308f592
gpiechnik2 <grzegorz.piechnik99@gmail.com>
2022-07-17 17:11:36 +0200

    Function updated. README updated

--- smtp.go
+++ smtp.go
@@ -13,19 +13,39 @@ func init() {
 
 type SMTP struct{}
 
+type options struct {
+	Subject string   `js:"subject"`
+	Message string   `js:"message"`
+	UDW     []string `js:"udw"`
+}
+
 func check(e error) {
 	if e != nil {
 		fmt.Println(e)
 	}
 }
 
-func plainAuth(host string, password string, fromMail string) smtp.Auth {
-	return smtp.PlainAuth("", fromMail, password, host)
+func plainAuth(host string, password string, sender string) smtp.Auth {
+	return smtp.PlainAuth("", sender, password, host)
 }
 
-func (*SMTP) SendMail(host string, port string, fromMail string, password string, udwList []string, message string) {
-	body := []byte(message)
-	auth := plainAuth(host, password, fromMail)
-	err := smtp.SendMail(host+":"+port, auth, fromMail, udwList, body)
+func (*SMTP) SendMail(host string, port string, sender string, password string, recipient string, options options) {
+	emailMessage := "From: " + sender + "\r\n" + "To: " + recipient + "\r\n"
+
+	if options.Subject != "" {
+		emailMessage += "Subject: " + options.Subject + "\r\n\r\n"
+	}
+
+	if options.Message != "" {
+		emailMessage += options.Message
+	}
+
+	if len(options.UDW) == 0 {
+		options.UDW = []string{recipient}
+	}
+
+	body := []byte(emailMessage)
+	auth := plainAuth(host, password, sender)
+	err := smtp.SendMail(host+":"+port, auth, sender, options.UDW, body)
 	check(err)
 }
